2cd91b1388a6b3587ac28b59b4a5375473272e91
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2022-12-16 16:17:57 +0500

    рефакторинг

--- Src/smooth.c
+++ Src/smooth.c
@@ -1,322 +1,52 @@
 #include "main.h"
 #include "smooth.h"
 
+static void output(uint8_t temper_cnt)
+{
+  const uint8_t period = 10;
+  uint8_t time_cnt = 0;
+
+  if (temper_cnt <= period)
+  {
+    time_cnt = period - temper_cnt;
+  }
+
+  for(uint8_t i = 0; i <= temper_cnt; i++) temper_out();
+  for(uint8_t i = 0; i <= time_cnt; i++) time_out();
+}
+
 void smooth_transition_time(void) // выполняеться за 280 мс
 {
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
-
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    time_out();
+    output(1);
+    output(1);
+    output(2);
+    output(2);
+    output(3);
+    output(4);
+    output(5);
+    output(5);
+    output(6);
+    output(7);
+    output(8);
+    output(8);
+    output(9);
+    output(9);
 }
 
 void smooth_transition_temp(void)
 {
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
-
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    time_out();
-    temper_out();
+    output(9);
+    output(9);
+    output(8);
+    output(8);
+    output(7);
+    output(6);
+    output(5);
+    output(5);
+    output(4);
+    output(3);
+    output(2);
+    output(2);
+    output(1);
+    output(1);
 }
