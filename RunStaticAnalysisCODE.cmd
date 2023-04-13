ceb5993e9d6455005ec57bf1966a2abeeaf25417
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2023-04-13 21:24:13 +0500

    Доработка скрипта

--- RunStaticAnalysisCODE.cmd
+++ RunStaticAnalysisCODE.cmd
@@ -5,7 +5,7 @@ rem Гайд: https://habr.com/ru/articles/210256/
 @echo on
 
 rem вывод результата в консоль
-cppcheck -q -j4 --enable=all --inconclusive ./Src
+cppcheck -q -j4 --enable=all --inconclusive -I ./Inc ./Src
 rem вывод результата в файл
 rem cppcheck -q -j4 --enable=all --inconclusive --output-file=checkReport.txt  ./Src
 
